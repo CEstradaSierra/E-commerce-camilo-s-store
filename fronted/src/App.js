d9273ff3e0c11bcc4e17ca1e302265b4d96afeb6
Camilo Estrada <camilo.estradas@udea.edu.co>
2023-08-17 14:38:03 -0500

    creating the fronted of my store

---
+++ fronted/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">Camilo's Store</a>
+
+      </header>
+      <main>
+        <p>list products</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
